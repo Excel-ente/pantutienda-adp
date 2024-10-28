d2157e833398e8a603294c8218e5600c011aac5c
kevinturkienich <turkienich@gmail.com>
2024-10-28 11:02:24 -0300

    ponienndo solo lectura campos de monitoreo

--- monitoreo/admin.py
+++ monitoreo/admin.py
@@ -4,3 +4,4 @@ from .models import FailedLoginAttempt
 @admin.register(FailedLoginAttempt)
 class FailedLoginAttemptAdmin(admin.ModelAdmin):
     list_display = ('ip_address','attempt_time')
+    readonly_fields = ('ip_address','attempt_time','ciudad','region','pais','organizacion')    

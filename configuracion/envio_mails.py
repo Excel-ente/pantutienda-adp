4817fef3c119d22d5019b8e23f56fef38210babe
kevinturkienich <turkienich@gmail.com>
2024-10-28 00:18:18 -0300

    cambios de metodo de envio de mail

--- configuracion/envio_mails.py
+++ configuracion/envio_mails.py
@@ -8,6 +8,7 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from django.template.loader import render_to_string
 import environ
+import json
 
 # Inicializa django-environ para leer el archivo .env
 env = environ.Env()
@@ -22,33 +23,17 @@ TEMPLATE_PATH_CLIENTE = os.path.join(os.path.dirname(__file__), 'mails/mail_alta
 
 def get_credentials():
     creds = None
-    token_json = os.getenv("GOOGLE_TOKEN")
-    #token_json = 'token.json'
-    
-    # Leer credenciales desde el archivo token.json si existe
-    if token_json:
-        creds = Credentials.from_authorized_user_file(token_json, SCOPES)
-    
-    # Si no existen credenciales o son inválidas, intenta refrescarlas
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            # Usa las variables de entorno para crear las credenciales
-            creds = Credentials(
-                None,
-                refresh_token=env('GOOGLE_OAUTH2_REFRESH_TOKEN'),
-                token_uri='https://oauth2.googleapis.com/token',
-                client_id=env('GOOGLE_OAUTH2_CLIENT_ID'),
-                client_secret=env('GOOGLE_OAUTH2_CLIENT_SECRET')
-            )
-            creds.refresh(Request())
-        # Guardar el nuevo token en token.json
-        with open(token_json, 'w') as token:
-            token.write(creds.to_json())
-    
-    return creds
+    token_json = os.getenv("GOOGLE_TOKEN")  # Cargar el token desde la variable de entorno
 
+    # Convertir el token en un objeto de credenciales si existe
+    if token_json:
+        creds = Credentials.from_authorized_user_info(json.loads(token_json), SCOPES)
+    
+    # Si las credenciales existen pero están vencidas, intenta refrescarlas
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+
+    return creds
 def send_mail_alta_proveedor(proveedor, email):
     try:
         # Obtener las credenciales de Gmail

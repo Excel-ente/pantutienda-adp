b4eecc466dfd0cc176fc0317f79f638aacb1d04d
kevinturkienich <turkienich@gmail.com>
2024-10-28 11:12:25 -0300

    cambioando list_view

--- monitoreo/admin.py
+++ monitoreo/admin.py
@@ -3,5 +3,5 @@ from .models import FailedLoginAttempt
 
 @admin.register(FailedLoginAttempt)
 class FailedLoginAttemptAdmin(admin.ModelAdmin):
-    list_display = ('ip_address','attempt_time')
+    list_display = ('ip_address','pais','attempt_time')
     readonly_fields = ('ip_address','attempt_time','ciudad','region','pais','organizacion')    
